87a1c44a0886fba0391680ef81742f62752d4a9c
Raphael Reventar <raphael.reventar@gmail.com>
2019-07-30 17:33:09 +0800

    renaming of scripts

---
+++ unity-sceneloader-extensions/Assets/Samples/Sample.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SceneLoaderExtensions;
+
+public class Sample : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            StartCoroutine(SceneLoader.LoadScenesSequentially("SampleScene1", "SampleScene2"));
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            StartCoroutine(SceneLoader.LoadScenesInParallel("SampleScene1", "SampleScene2"));
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            StartCoroutine(SceneLoader.UnloadScenesSequentially("SampleScene1", "SampleScene2"));
+
+        }
+        else if (Input.GetKeyDown(KeyCode.F))
+        {
+            StartCoroutine(SceneLoader.UnloadScenesInParallel("SampleScene1", "SampleScene2"));
+        }
+    }
+}
--- unity-sceneloader-extensions/Assets/sceneloader-extensions/SceneLoaderExtensions.cs
+++ unity-sceneloader-extensions/Assets/sceneloader-extensions/SceneLoaderExtensions.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace SceneManagerExtensions
+namespace SceneLoaderExtensions
 {
     public static class SceneLoader
     {
